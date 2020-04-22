5439e8a1b9cf43f08b3a810d0627d9ef4c11fbdf
has387 <has387@gmail.com>
2020-04-22 16:49:40 -0600

    update guessinggame

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,9 @@
 files=$(ls -p | grep -v / | wc -l)
+
 echo "Welcome to the GuessingGame!"
 echo "How many files are there in your current directory? let's guess!"
 read -p "Please enter your guess: " guess
+
 function updown {
 ans=$1
 ges=$2
@@ -13,10 +15,12 @@ ges=$2
 		echo "too high"
 	fi
 }
+
 while [[ $guess != $files ]]
 do
 	echo "You entered a wrong number."
 	read -p "Your number is $(updown $files $guess). Please enter a new guess: " guess
 done
+
 echo "Congraturation!"
 echo "You guessed right! Game is over now"
