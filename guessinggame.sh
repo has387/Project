e3354a80b49c15ef37279aa213c6754482fb46ba
has387 <has387@gmail.com>
2020-04-22 16:37:47 -0600

    Updated

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,7 +1,7 @@
 files=$(ls -p | grep -v / | wc -l)
 echo "Welcome to the GuessingGame!"
 echo "How many files are there in your current directory? let's guess!"
-read -p "Please enter your guess:" guess
+read -p "Please enter your guess: " guess
 function updown {
 ans=$1
 ges=$2
@@ -16,7 +16,7 @@ ges=$2
 while [[ $guess != $files ]]
 do
 	echo "You entered a wrong number."
-	read -p "Your number is $(updown $files $guess). Please enter a new guess:" guess
+	read -p "Your number is $(updown $files $guess). Please enter a new guess: " guess
 done
 echo "Congraturation!"
 echo "You guessed right! Game is over now"
